e01587b2df3e7b5b4a47b5708168bc862195055e
jd-otero <BCrown@2099>
2023-09-27 14:17:48 -0500

    Update filter_data
    Make function easier internally and to the user

--- R/filter_data.R
+++ R/filter_data.R
@@ -1,11 +1,10 @@
-
 #' Filter MGN data
 #'
 #' @param .data dataframe containing MGN - CNPV data
 #' @param level numeric, either 1 for department or 2 for municipality level
-#' @param filter list containing administrative divisions to be included and
-#' columns to select from dataframe
 #' @param include_geometry boolean to include (or not) the geometry.
+#' @param ad character vector with municipalities to filter
+#' @param columns character vector with columns to filter
 #'
 #' @return sf dataframe containing MGN(National Geo statistical Frame) and CNPV
 #' (National Population and Living Census) data
@@ -13,73 +12,60 @@
 #' @export
 filter_mgn_cnpv <- function(.data,
                             level,
-                            filter = list(AD = "all", columns = "all"),
+                            ad = NULL,
+                            columns = NULL,
                             include_geometry = TRUE) {
   checkmate::assert_class(.data, c("sf", "data.frame"))
   checkmate::assert_choice(level, c(1, 2))
-  checkmate::assert_list(filter)
   checkmate::assert_logical(include_geometry)
-  
-  if (is.null(filter$columns)) {
-    columns <- "all"
-  } else {
-    columns <- filter$columns
-  }
-  
-  if (is.null(filter$AD)) {
-    rows <- "all"
-  } else {
-    rows <- filter$AD
-  }
-  
+  checkmate::assert_character(ad, null.ok = TRUE)
+  checkmate::assert_character(columns, null.ok = TRUE)
+
   census <- .data
   if (!include_geometry) {
     census <- sf::st_drop_geometry(census)
   }
+  if (!is.null(columns) && !all(columns %in% colnames(census))) {
+    stop("Specified column names are not included in the dataset")
+  }
   if (level == 2) {
     # check column names
-    if (columns != "all" && !all(columns %in% colnames(census))) {
-      stop("Specified column names are not included in the MGNCNPV02 dataset")
-    } else if (columns == "all") {
-      columns <- colnames(census)
-    }
-    if (all(rows %in% census$MPIO_CDPMP)) {
+    if (all(ad %in% census$MPIO_CDPMP)) {
       census <- census %>%
-        dplyr::filter(.data$MPIO_CDPMP %in% rows) %>%
+        dplyr::filter(.data$MPIO_CDPMP %in% ad) %>%
         dplyr::select(dplyr::all_of(c(
-          "MPIO_CDPMP", columns)))
-    } else if (all(rows %in% census$MPIO_CNMBR)) {
+          "MPIO_CDPMP", columns
+        )))
+    } else if (all(ad %in% census$MPIO_CNMBR)) {
       census <- census %>%
-        dplyr::filter(.data$MPIO_CNMBR %in% rows) %>%
+        dplyr::filter(.data$MPIO_CNMBR %in% ad) %>%
         dplyr::select(dplyr::all_of(c(
-          "MPIO_CNMBR", columns)))
-    } else if (all(rows == "all")) {
+          "MPIO_CNMBR", columns
+        )))
+    } else if (is.null(ad)) {
       census <- census %>% dplyr::select(dplyr::all_of(c(
-        "MPIO_CDPMP", columns)))
+        "MPIO_CDPMP", "MPIO_CNMBR", columns
+      )))
     } else {
       stop("Some municipalities could not be found, please check your input")
     }
   } else if (level == 1) {
-    # check column names
-    if (columns != "all" && !all(columns %in% colnames(census))) {
-      stop("Specified column names are not included in the MGNCNPV01 dataset")
-    } else if (columns == "all") {
-      columns <- colnames(census)
-    }
-    
-    if (all(rows %in% census$DPTO_CCDGO)) {
+    if (all(ad %in% census$DPTO_CCDGO)) {
       census <- census %>%
-        dplyr::filter(.data$DPTO_CCDGO %in% rows) %>%
+        dplyr::filter(.data$DPTO_CCDGO %in% ad) %>%
         dplyr::select(dplyr::all_of(c(
-          "DPTO_CCDGO", columns)))
-    } else if (all(rows %in% census$DPTO_CNMBR)) {
+          "DPTO_CCDGO", columns
+        )))
+    } else if (all(ad %in% census$DPTO_CNMBR)) {
       census <- census %>%
-        dplyr::filter(.data$DPTO_CNMBR %in% rows) %>%
+        dplyr::filter(.data$DPTO_CNMBR %in% ad) %>%
         dplyr::select(dplyr::all_of(c(
-          "DPTO_CNMBR", columns)))
-    } else if (all(rows == "all")) {
+          "DPTO_CNMBR", columns
+        )))
+    } else if (is.null(ad)) {
       census <- census %>% dplyr::select(dplyr::all_of(c(
-        "DPTO_CCDGO", columns)))
+        "DPTO_CCDGO", "DPTO_CNMBR", columns
+      )))
     } else {
       stop("Some departments could not be found, please check your input")
     }
