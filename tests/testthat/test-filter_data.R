70aeba41b9be20341b1d5aeb81f6b920ef6ecc51
jd-otero <julian-otero@outlook.com>
2023-09-26 19:41:15 -0500

    Update tests

---
+++ tests/testthat/test-filter_data.R
@@ -0,0 +1,27 @@
+
+
+dataset_1 <- download("MGNCNPV01")
+dataset_2 <- download("MGNCNPV02")
+test_that("Filter MGN-CNPV errors are thrown", {
+  expect_error(filter_mgn_cnpv(.data = dataset_1, level = 4))
+  expect_error(filter_mgn_cnpv(.data = dataset_1, include_geometry = 1))
+  expect_error(filter_mgn_cnpv(.data = dataset_1, level = 1, filter = c("73001", "05001")))
+  expect_error(filter_mgn_cnpv(.data = dataset_1))
+  expect_error(filter_mgn_cnpv(.data = dataset_1, level = 1, filter = list(
+    AD = c("05", "73"),
+    columns = c("1")
+  )))
+  expect_error(filter_mgn_cnpv(.data = dataset_2, level = 2, filter = list(
+    AD = c("05", "73"),
+    columns = c("1")
+  )))
+})
+
+test_that("Filter MGN-CNPV works as expected", {
+  expect_s3_class(filter_mgn_cnpv(.data = dataset_1, level = 1, include_geometry = FALSE), "data.frame")
+  expect_s3_class(filter_mgn_cnpv(.data = dataset_2, level = 2, include_geometry = FALSE), "data.frame")
+  expect_s3_class(filter_mgn_cnpv(.data = dataset_1, level = 1), "data.frame")
+  expect_s3_class(filter_mgn_cnpv(.data = dataset_2, level = 2), "data.frame")
+  expect_s3_class(filter_mgn_cnpv(.data = dataset_1, level = 1, filter = list(AD = c("CUNDINAMARCA", "META"))), "data.frame")
+  expect_s3_class(filter_mgn_cnpv(.data = dataset_2, level = 2, filter = list(AD = c("RIONEGRO", "MARINILLA"))), "data.frame")
+})
