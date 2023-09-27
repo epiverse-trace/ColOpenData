802b9d3bd7f71e5f5643f266a9d2d87fa172cac0
jd-otero <BCrown@2099>
2023-09-27 14:17:48 -0500

    Update filter_data
    Make function easier internally and to the user

--- tests/testthat/test-filter_data.R
+++ tests/testthat/test-filter_data.R
@@ -1,53 +1,79 @@
-
-
 dataset_1 <- download("MGNCNPV01")
 dataset_2 <- download("MGNCNPV02")
 test_that("Filter MGN-CNPV errors are thrown", {
-  expect_error(filter_mgn_cnpv(.data = dataset_1, 
-                               level = 4))
-  expect_error(filter_mgn_cnpv(.data = dataset_1, 
-                               include_geometry = 1))
-  expect_error(filter_mgn_cnpv(.data = dataset_1, 
-                               level = 1, 
-                               filter = c("73001", "05001")))
+  expect_error(filter_mgn_cnpv(
+    .data = dataset_1,
+    level = 4
+  ))
+  expect_error(filter_mgn_cnpv(
+    .data = dataset_1,
+    include_geometry = 1
+  ))
+  expect_error(filter_mgn_cnpv(
+    .data = dataset_1,
+    level = 1,
+    ad = c("73001", "05001")
+  ))
   expect_error(filter_mgn_cnpv(.data = dataset_1))
-  expect_error(filter_mgn_cnpv(.data = dataset_1, 
-                               level = 1, 
-                               filter = list(
-                                 AD = c("05", "73"),
-                                 columns = c("1", "2")
-  )))
-  expect_error(filter_mgn_cnpv(.data = dataset_2, 
-                               level = 2, 
-                               filter = list(
-                                 AD = c("05", "73"),
-                                 columns = c("1", "THIS")
-  )))
+  expect_error(filter_mgn_cnpv(
+    .data = dataset_1,
+    level = 1,
+    ad = c("05", "73"),
+    columns = c("1", "2")
+  ))
+  expect_error(filter_mgn_cnpv(
+    .data = dataset_2,
+    level = 2,
+    ad = c("05", "73"),
+    columns = c("1", "THIS")
+  ))
 })
 
 test_that("Filter MGN-CNPV works as expected", {
-  expect_s3_class(filter_mgn_cnpv(.data = dataset_1, 
-                                  level = 1, 
-                                  include_geometry = FALSE), 
-                  "data.frame")
-  expect_s3_class(filter_mgn_cnpv(.data = dataset_2, 
-                                  level = 2, 
-                                  include_geometry = FALSE), 
-                  "data.frame")
-  expect_s3_class(filter_mgn_cnpv(.data = dataset_1, 
-                                  level = 1), 
-                  "data.frame")
-  expect_s3_class(filter_mgn_cnpv(.data = dataset_2, 
-                                  level = 2), 
-                  "data.frame")
-  expect_s3_class(filter_mgn_cnpv(.data = dataset_1, 
-                                  level = 1, 
-                                  filter = list(
-                                    AD = c("CUNDINAMARCA", "META"))), 
-                  "data.frame")
-  expect_s3_class(filter_mgn_cnpv(.data = dataset_2, 
-                                  level = 2, filter = 
-                                    list(
-                                      AD = c("RIONEGRO", "MARINILLA"))), 
-                  "data.frame")
+  expect_s3_class(
+    filter_mgn_cnpv(
+      .data = dataset_1,
+      level = 1,
+      include_geometry = FALSE
+    ),
+    "data.frame"
+  )
+  expect_s3_class(
+    filter_mgn_cnpv(
+      .data = dataset_2,
+      level = 2,
+      include_geometry = FALSE
+    ),
+    "data.frame"
+  )
+  expect_s3_class(
+    filter_mgn_cnpv(
+      .data = dataset_1,
+      level = 1
+    ),
+    "data.frame"
+  )
+  expect_s3_class(
+    filter_mgn_cnpv(
+      .data = dataset_2,
+      level = 2
+    ),
+    "data.frame"
+  )
+  expect_s3_class(
+    filter_mgn_cnpv(
+      .data = dataset_1,
+      level = 1,
+      ad = c("CUNDINAMARCA", "META")
+    ),
+    "data.frame"
+  )
+  expect_s3_class(
+    filter_mgn_cnpv(
+      .data = dataset_2,
+      level = 2,
+      ad = c("RIONEGRO", "MARINILLA")
+    ),
+    "data.frame"
+  )
 })
