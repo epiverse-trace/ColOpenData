c044621291346184fadd83dd5d1ef0696ec2b4c6
jd-otero <BCrown@2099>
2023-10-04 16:28:04 -0500

    Include sample data documentation

---
+++ R/dengue_2018.R
@@ -0,0 +1,12 @@
+#' Dengue data 2018
+#'
+#' Count of Dengue cases in Colombia for 2021 grouped at department level
+#'
+#' @format ## `dengue_2018`
+#' data frame with 33 rows and 2 columns:
+#' \describe{
+#'   \item{cod_dpto_o}{Department code}
+#'   \item{cases}{Dengue case count}
+#' }
+#' @source <https://epiverse-trace.github.io/sivirep/>
+"dengue_2018"
