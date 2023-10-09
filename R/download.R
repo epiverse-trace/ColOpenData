8aef6eccb622a44b266d81e9e035a55971a53831
jd-otero <julian-otero@outlook.com>
2023-10-09 16:18:25 -0500

    Update dependencies

--- R/download.R
+++ R/download.R
@@ -10,7 +10,7 @@
 download <- function(dataset) {
   path <- retrieve_path(dataset)
   downloaded_data <- retrieve_dataset(path)
-  if (grep("DPDPP", dataset)) {
+  if (grepl("DPDPP", dataset)) {
     downloaded_data <- clean_ddp_pp(downloaded_data)
   }
   return(downloaded_data)
@@ -31,33 +31,34 @@ download <- function(dataset) {
 #' }
 #' @keywords internal
 clean_ddp_pp <- function(dataset) {
-  pp_prep <- na.omit(dataset)
+  pp_prep <- stats::na.omit(dataset)
   max_col <- ncol(pp_prep)
   min_col <- 5
   if ("DPMP" %in% colnames(pp_prep)) {
     min_col <- 7
   }
-
+  pp_prep_filtered <- janitor::clean_names(pp_prep_filtered)
   pp_prep_filtered <- pp_prep %>% tidyr::pivot_longer(
     cols = seq(min_col, max_col),
     names_to = "SEXO_EDAD",
     values_to = "CONTEO"
   )
   pp_prep_filtered <- pp_prep_filtered %>% dplyr::mutate(
-    SEXO = unlist(stringr::str_split(SEXO_EDAD, "_", simplify = T))[, 1],
-    EDAD = unlist(stringr::str_split(SEXO_EDAD, "_", simplify = T))[, 2]
+    SEXO = unlist(stringr::str_split(.data$SEXO_EDAD, "_", simplify = T))[, 1],
+    EDAD = unlist(stringr::str_split(.data$SEXO_EDAD, "_", simplify = T))[, 2]
   )
-  if ("DPMP" %in% colnames(pp_g_f)) {
-    pp_out <- pp_prep_filtered %>% dplyr::select(c(
-      "DP", "DPNOM", "MPIO", "DPMP", "AÑO",
-      "ÁREA GEOGRÁFICA", "SEXO", "EDAD",
+  if ("DPMP" %in% colnames(pp_prep_filtered)) {
+    pp_out <- pp_prep_filtered %>% dplyr::select(janitor::make_clean_names(c(
+      "DP", "DPNOM", "MPIO", "DPMP", "ANO",
+      "AREA GEOGRAFICA", "SEXO", "EDAD",
       "CONTEO"
-    ))
+    )))
   } else {
-    pp_out <- pp_prep_filtered %>% dplyr::select(c(
-      "DP", "DPNOM", "AÑO", "ÁREA GEOGRÁFICA",
+    pp_out <- pp_prep_filtered %>% dplyr::select(janitor::make_clean_names(c(
+      "DP", "DPNOM", "ANO", "AREA GEOGRAFICA",
       "SEXO", "EDAD", "CONTEO"
-    ))
+    )))
   }
+  colnames(pp_out) <- toupper(colnames(pp_out))
   return(pp_out)
 }
