39cf07f4cc9642c5e81f4311b2ccfaafdd063595
jd-otero <julian-otero@outlook.com>
2023-11-15 13:20:05 -0500

    Include weather from IDEAM
    Include functions to
    - Get IDEAM stations in a given ROI
    - Retrieve data for IDEAM stations
    - Aggregate data for each ROI or municipality
    - Plot output

---
+++ R/weather_IDEAM.R
@@ -0,0 +1,255 @@
+#' Retrieve weather data by municipality name
+#'
+#' @param name string of the DIVIPOLA code for a given municipality
+#' @param start_date starting date in format "YYYY-MM-DD"
+#' @param end_date end date in format "YYYY-MM-DD"
+#' @param frequency aggregation frequency. Can be "day", "week","month" or
+#' "year"
+#' @param tag character containing tags to analyze
+#' @param plot if TRUE, plot the individual stations that contain data
+#' @param group if TRUE, returns only one observation from the mean of the
+#' stations consulted
+#'
+#' @return data.frame with the observed data for the given municipality
+weather_stations_mpio <- function(name, start_date, end_date, frequency, tag,
+                                   plot = FALSE, group = FALSE) {
+  checkmate::assert_character(name)
+  checkmate::assert_character(start_date)
+  checkmate::assert_character(end_date)
+  checkmate::assert_choice(frequency, c("day", "week", "month", "year"))
+  checkmate::assert_choice(tag, c(
+    "TSSM_CON", "THSM_CON", "TMN_CON", "TMX_CON",
+    "TSTG_CON", "HR_CAL", "HRHG_CON", "TV_CAL",
+    "TPR_CAL", "PTPM_CON", "EVTE_CON", "FA_CON",
+    "NB_CON", "RCAM_CON", "BSHG_CON", "VVAG_CON",
+    "DVAG_CON", "VVMXAG_CON", "DVMXAG_CON"
+  ))
+
+  mpios <- ColOpenData::download("DANE_MGNCNPV_2018_MPIO")
+  mpio <- mpios[which(mpios$MPIO_CDPMP == name), ]
+  working_stations <- weather_stations(
+    mpio, start_date, end_date,
+    frequency, tag, plot, group
+  )
+  return(working_stations)
+}
+
+#' Retrieve weather stations
+#' @description
+#' Retrieve weather stations with observed data in a given geometry.
+#'
+#' @param geometry sf object containing a geometry (polygon or multipolygon)
+#' @param start_date starting date in format "YYYY-MM-DD"
+#' @param end_date end date in format "YYYY-MM-DD"
+#' @param frequency aggregation frequency. Can be "day", "week","month" or "year"
+#' @param tag character containing tags to analyze
+#' @param plot if TRUE, plot the individual stations that contain data
+#' @param group if TRUE, returns only one observation from the mean of the
+#' stations consulted
+#'
+#' @return data.frame with the observed data for the given geometry
+#' @export
+weather_stations <- function(geometry, start_date, end_date, frequency,
+                             tag, plot = FALSE, group = FALSE) {
+  checkmate::assert_class(geometry, "sf")
+  checkmate::assert_character(start_date)
+  checkmate::assert_character(end_date)
+  checkmate::assert_choice(frequency, c("day", "week", "month", "year"))
+  checkmate::assert_choice(tag, c(
+    "TSSM_CON", "THSM_CON", "TMN_CON", "TMX_CON",
+    "TSTG_CON", "HR_CAL", "HRHG_CON", "TV_CAL",
+    "TPR_CAL", "PTPM_CON", "EVTE_CON", "FA_CON",
+    "NB_CON", "RCAM_CON", "BSHG_CON", "VVAG_CON",
+    "DVAG_CON", "VVMXAG_CON", "DVMXAG_CON"
+  ))
+
+  stations_roi <- stations_in_roi(geometry)
+  stations <- stations_roi$codigo
+  working_stations <- retrieve_working_stations(
+    stations, start_date, end_date,
+    frequency, tag, plot, group
+  )
+  return(working_stations)
+}
+
+#' Retrieve stations that took measurements in the given dates
+#' @description
+#' Retrieves, from a list of stations, the ones that contain observations for
+#' the given tag in specified dates. Might include stations that are no longer
+#' working.
+#'
+#' @param stations vector containing the stations codes
+#' @param start_date starting date in format "YYYY-MM-DD"
+#' @param end_date end date in format "YYYY-MM-DD"
+#' @param frequency aggregation frequency. Can be "day", "week","month" or
+#' "year"
+#' @param tag character containing tags to analyze
+#' @param plot if TRUE, plot the individual stations that contain data
+#' @param group if TRUE, returns only one observation from the mean of the
+#'
+#' @importFrom rlang .data
+#' 
+#' @return data.frame containing the observed data for the given stations
+#' @export
+retrieve_working_stations <- function(stations, start_date, end_date,
+                                      frequency, tag, plot = FALSE,
+                                      group = FALSE) {
+  checkmate::assert_vector(stations)
+  checkmate::assert_character(start_date)
+  checkmate::assert_character(end_date)
+  checkmate::assert_choice(frequency, c("day", "week", "month", "year"))
+  checkmate::assert_choice(tag, c(
+    "TSSM_CON", "THSM_CON", "TMN_CON", "TMX_CON",
+    "TSTG_CON", "HR_CAL", "HRHG_CON", "TV_CAL",
+    "TPR_CAL", "PTPM_CON", "EVTE_CON", "FA_CON",
+    "NB_CON", "RCAM_CON", "BSHG_CON", "VVAG_CON",
+    "DVAG_CON", "VVMXAG_CON", "DVMXAG_CON"
+  ))
+  paths_stations <- paste0(tag, "@", stations, ".data")
+  dates <- seq(as.Date(start_date), as.Date(end_date), by = frequency)
+  floor_dates <- lubridate::floor_date(dates, unit = frequency)
+  output <- data.frame(dates = floor_dates)
+  for (i in 1:length(paths_stations)) {
+    if (file.exists(file.path(
+      "C:/Users/Julia/Downloads/BACKUP DATOS HASTA MAYO 2023",
+      paths_stations[i]
+    ))) {
+      temp_data <- readr::read_delim(
+        file.path(
+          "C:/Users/Julia/Downloads/BACKUP DATOS HASTA MAYO 2023",
+          paths_stations[i]
+        ),
+        delim = "|", escape_double = FALSE, trim_ws = TRUE,
+        show_col_types = FALSE
+      )
+      temp_data$Fecha <- as.POSIXct(temp_data$Fecha,
+        format = "%Y-%m-%d %H:%M:%S", tz = "UTC"
+      )
+      filtered <- temp_data %>%
+        dplyr::filter(
+          .data$Fecha >= start_date,
+          .data$Fecha <= (as.Date(end_date) + 1)
+        )
+      if (frequency == "week") {
+        filtered <- filtered %>%
+          dplyr::mutate(week = lubridate::floor_date(.data$Fecha, unit = "week")) %>%
+          dplyr::group_by(.data$week) %>%
+          dplyr::summarise(value = round(mean(.data$Valor), 2))
+      } else if (frequency == "month") {
+        filtered <- filtered %>%
+          dplyr::mutate(month = lubridate::floor_date(.data$Fecha, unit = "month")) %>%
+          dplyr::group_by(.data$month) %>%
+          dplyr::summarise(value = round(mean(.data$Valor), 2))
+      } else if (frequency == "year") {
+        filtered <- filtered %>%
+          dplyr::mutate(year = lubridate::floor_date(.data$Fecha, unit = "year")) %>%
+          dplyr::group_by(.data$year) %>%
+          dplyr::summarise(value = round(mean(.data$Valor), 2))
+      } else {
+        filtered <- filtered %>%
+          dplyr::group_by(as.Date(.data$Fecha)) %>%
+          dplyr::summarise(value = round(mean(.data$Valor), 2))
+      }
+      if (all(is.na(filtered))) {
+        next
+      }
+      names(filtered) <- c("dates", paste("station", stations[i], sep = "_"))
+      output <- merge(output, filtered,
+        by.x = "dates", by.y = "dates",
+        all.x = TRUE
+      )
+    }
+  }
+  if (ncol(output) == 1) {
+    stop("There were no records for the given ROI and dates")
+  }
+
+  if (group & ncol(output) > 2) {
+    output <- group_stations(output, tag)
+  }
+  if (plot) {
+    plot_stations(output, tag)
+  }
+  return(output)
+}
+
+#' Stations in region of interest
+#' @description
+#' Retrieves the stations contained inside a given geometry
+#'
+#' @param geometry sf object containing a geometry (polygon or multipolygon)
+#'
+#' @return data.frame with the stations inside the consulted geometry
+#' @export
+stations_in_roi <- function(geometry) {
+  checkmate::assert_class(geometry, "sf")
+  crs <- sf::st_crs(geometry)
+  IDEAM_stations <- utils::read.csv(
+    "C:/Users/Julia/OneDrive - Universidad de los Andes/TRACE/ColOpenData/supportR/IDEAM_stations.csv"
+  )
+  geo_stations <- sf::st_as_sf(IDEAM_stations,
+    coords = c("longitude", "latitude"),
+    remove = F
+  )
+  geo_stations <- sf::st_set_crs(geo_stations, crs)
+  intersections <- sf::st_within(geo_stations, geometry, sparse = FALSE)
+
+  filtered_stations <- geo_stations[which(intersections == TRUE), ]
+  if (nrow(filtered_stations) == 0) {
+    stop("There are no stations in the given ROI")
+  }
+  return(filtered_stations)
+}
+
+#' Group stations
+#' @description
+#' Groups stations by the mean of the observations
+#'
+#' @param stations_df data.frame containing stations to group
+#' @param tag string with the tag consulted
+#'
+#' @return a data.frame with only two columns containing the dates and the
+#' grouped observations
+#' @keywords internal
+group_stations <- function(stations_df, tag) {
+  if (ncol(stations_df) == 2) {
+    output <- stations_df
+    names(output) <- c("dates", tag)
+  } else {
+    output <- as.data.frame(stations_df$dates)
+    output[tag] <- round(rowMeans(stations_df[, -c(1)], na.rm = TRUE), 2)
+    names(output) <- c("dates", tag)
+  }
+  return(output)
+}
+
+#' Plot stations
+#' @description
+#' Plot weather stations if required
+#'
+#' @param stations_df data.frame with stations containing observations for each
+#' date
+#' @param tag string with the tag consulted
+#'
+#' @keywords internal
+plot_stations <- function(stations_df, tag) {
+  # plot max 10
+  cols <- min(ncol(stations_df) - 1, 10)
+  if (cols > 10) {
+    warning("Only first 10 stations are plotted")
+  }
+  if (cols > 1) {
+    graphics::par(mfrow = c(2, min(ceiling(cols / 2), 5)))
+    for (i in 1:cols) {
+      plot(stations_df[, 1], stations_df[, i + 1],
+        xlab = "date", ylab = tag, main = names(stations_df)[i + 1],
+        type = "l"
+      )
+    }
+  } else {
+    plot(stations_df[, 1], stations_df[, 2],
+      xlab = "date", ylab = tag, main = names(stations_df)[2],
+      type = "l"
+    )
+  }
+}
