a0a803169d3a71b5ae1dc41c0a36e25e1440d2b6
jd-otero <julian-otero@outlook.com>
2024-01-26 14:03:14 -0500

    Update dictionary function

--- tests/testthat/test-dictionaries.R
+++ tests/testthat/test-dictionaries.R
@@ -1,8 +1,8 @@
 test_that("Dictionary errors are thrown", {
-  expect_error(data_dictionary("DANE_MGNCNPV_2018"))
-  expect_error(data_dictionary(4))
+  expect_error(dictionary("DANE_MGNCNPV_2018"))
+  expect_error(dictionary(4))
 })
 
 test_that("Dictionary works as expected", {
-  expect_s3_class(data_dictionary("DANE_MGNCNPV_2018_DPTO"), "data.frame")
+  expect_s3_class(dictionary("DANE_MGNCNPV_2018_DPTO"), "data.frame")
 })
