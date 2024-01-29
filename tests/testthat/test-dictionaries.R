ad7ade30f71f00055786c6b5a8105b4e9fca72f1
jd-otero <julian-otero@outlook.com>
2024-01-29 13:07:11 -0500

    Include list datasets tests

--- tests/testthat/test-dictionaries.R
+++
@@ -1,8 +0,0 @@
-test_that("Dictionary errors are thrown", {
-  expect_error(dictionary("DANE_MGNCNPV_2018"))
-  expect_error(dictionary(4))
-})
-
-test_that("Dictionary works as expected", {
-  expect_s3_class(dictionary("DANE_MGNCNPV_2018_DPTO"), "data.frame")
-})
