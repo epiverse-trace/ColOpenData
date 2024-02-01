43e0ec5a4c6c66b6f22bcb92732554bc431f2bfe
jd-otero <BCrown@2099>
2024-02-01 15:05:16 -0500

    Include demographic csv
    Include all demographic csv files in config file

--- R/download_demographic.R
+++ R/download_demographic.R
@@ -30,11 +30,12 @@ retrieve_demographic_dataset <- function(dataset_path, sheet) {
   tryCatch(
     {
       dataset <- retrieve_table(dataset_path, sep = ";")
+      dataset <- dataset[, -c(1,2)] # Remove index (temporary)
     },
     error = function(e) {
       stop("`dataset` not found")
     }
   )
-  # nolint end
+  
   return(dataset)
 }
--- tests/testthat/test-download_demographic.R
+++ tests/testthat/test-download_demographic.R
@@ -4,5 +4,5 @@ test_that("Download demographic errors are thrown", {
 
 test_that("Download demographic works", {
   expect_s3_class(download_demographic("DANE_CNPVV_2018_10D"), "data.frame")
-  expect_length(download_demographic("DANE_CNPVH_2018_1HD"), 7L)
+  expect_length(download_demographic("DANE_CNPVH_2018_1HD"), 5L)
 })
