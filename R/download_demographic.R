d04072f8d1f51a54b66bf67db324d35aec8a2a19
jd-otero <BCrown@2099>
2024-02-09 12:49:06 -0500

    Remove sheets parameter
    Since Excel files are no longer used, sheet parameter will no longer be needed.

--- R/download_demographic.R
+++ R/download_demographic.R
@@ -21,16 +21,14 @@ download_demographic <- function(dataset) {
 #' Retrieve demographic dataset from path
 #'
 #' @param dataset_path path to the dataset on repository
-#' @param sheet string to indicate specific sheet.
 #'
 #' @return consulted dataset.
 #'
 #' @keywords internal
-retrieve_demographic_dataset <- function(dataset_path, sheet) {
+retrieve_demographic_dataset <- function(dataset_path) {
   tryCatch(
     {
       dataset <- retrieve_table(dataset_path, sep = ";")
-      dataset <- dataset[, -c(1, 2)] # Remove index (temporary)
     },
     error = function(e) {
       stop("`dataset` not found")
