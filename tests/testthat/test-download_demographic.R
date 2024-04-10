1956de72d1034249a9773aa68daa4d28cea385c8
M. Camila Tavera-Cifuentes <88789311+macataci@users.noreply.github.com>
2024-04-10 12:21:58 -0500

    Update test-download_demographic.R

--- tests/testthat/test-download_demographic.R
+++ tests/testthat/test-download_demographic.R
@@ -1,5 +1,6 @@
 test_that("Download demographic errors are thrown", {
   expect_error(download_demographic("MGNCNPV06"))
+  expect_error(download_demographic("DANE_MGN_2018_DPTO"))
   expect_error(download_demographic())
 })
 
