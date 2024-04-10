01644ee86cee4ffed1ea8bed52b262cc8fa38db5
M. Camila Tavera-Cifuentes <88789311+macataci@users.noreply.github.com>
2024-04-10 12:32:13 -0500

    Update test-download_geospatial.R

--- tests/testthat/test-download_geospatial.R
+++ tests/testthat/test-download_geospatial.R
@@ -1,6 +1,7 @@
 test_that("Download geospatial errors are thrown", {
-  expect_error(download_geospatial("MGNCNPV06"))
   expect_error(download_geospatial())
+  expect_error(download_geospatial("MGNCNPV06"))
+  expect_error(download_geospatial("DANE_MGN_2019_DPTO"))
   expect_error(download_geospatial(
     dataset = "DANE_MGN_2018_DPTO",
     include_geom = FALSE,
@@ -9,11 +10,6 @@ test_that("Download geospatial errors are thrown", {
 })
 
 test_that("Download geospatial works as expected", {
-  expect_snapshot(download_geospatial(
-    dataset = "DANE_MGN_2018_DPTO",
-    include_geom = TRUE,
-    include_cnpv = TRUE
-  ))
   expect_snapshot(download_geospatial(
     dataset = "DANE_MGN_2018_DPTO",
     include_geom = TRUE,
