7dc119866897a82625e32bc05ad32cc5fbce4434
macataci <mc.tavera@uniandes.edu.co>
2024-05-11 14:03:59 -0500

    updates

--- tests/testthat/test-merge_geo_dem.R
+++ tests/testthat/test-merge_geo_dem.R
@@ -18,10 +18,12 @@ test_that("Merge geospational demographic errors are thrown", {
 test_that("Merge geospational demographic works as expected", {
   expect_s3_class(merge_geo_dem(
     spatial_level = "department",
-    "DANE_CNPVH_2018_1HD", "condicion"
+    dem_dataset = "DANE_CNPVH_2018_1HD", 
+    column = "condicion"
   ), "data.frame")
   expect_snapshot(merge_geo_dem(
     spatial_level = "municipality",
-    "DANE_CNPVH_2018_1HM", "condicion"
+    dem_dataset = "DANE_CNPVH_2018_1HM", 
+    column = "condicion"
   ))
 })
