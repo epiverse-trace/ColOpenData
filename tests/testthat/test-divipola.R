2e5d1d86a9f8357425a71aa458a3a74786b1cedf
jd-otero <julian-otero@outlook.com>
2024-05-23 11:53:42 -0500

    Update snapshots

--- tests/testthat/test-divipola.R
+++ tests/testthat/test-divipola.R
@@ -5,14 +5,14 @@ test_that("Divipola department code throws errors", {
 
 test_that("Divipola department code works as expected", {
   expect_identical(
-    divipola_department_code(c("TOLIMA", "ANTIOQUIA")),
+    divipola_department_code(c("Tolima", "Antioquia")),
     c("73", "05")
   )
   expect_identical(
     divipola_department_code(c(
-      "SANTANDER",
+      "Santander",
       "NORTE DE SANTANDER",
-      "SAN ANDRES"
+      "san andres"
     )),
     c("68", "54", "88")
   )
@@ -21,40 +21,40 @@ test_that("Divipola department code works as expected", {
 test_that("Divipola municipality code throws errors", {
   expect_error(divipola_municipality_code(
     c(6787, 13),
-    c("SOPLAVIENTO", "CARTAGENA")
+    c("Soplaviento", "Cartagena")
   ))
   expect_error(divipola_municipality_code(
-    c("BOLIVAR", "BOLIVAR"),
+    c("Bolivar", "Bolivar"),
     c(444, 555)
   ))
   expect_error(divipola_municipality_code(
-    "BOLIVAR",
-    c("SOPLAVIENTO", "CARTAGENA")
+    "Bolivar",
+    c("Soplaviento", "Cartagena")
   ))
   expect_warning(divipola_municipality_code(
-    c("BOLIVAR", "CARTAGENA"),
-    c("SOPLAVIENTO", "CARTAGENA")
+    c("Bolivar", "Panamá"),
+    c("Soplaviento", "Cartagena")
   ))
   expect_warning(divipola_municipality_code(
-    c("BOLIVAR", "BOLIVAR"),
-    c("SOPLAVIENTO", "SANTANDER")
+    c("Bolivar", "Bolivar"),
+    c("S", "Cartagena")
   ))
 })
 
 test_that("Divipola municipality code works as expected", {
   expect_identical(
     divipola_municipality_code(
-      c("SANTANDER", "ANTIOQUIA"),
-      c("RIONEGRO", "RIONEGRO")
+      c("Santander", "Antioquia"),
+      c("Rionegro", "Rionegro")
     ),
     c("68615", "05615")
   )
   expect_identical(
     divipola_municipality_code(
-      c("ANTIOQUIA", "ANTIOQUIA"),
+      c("Antioquia", "Antioquia"),
       c(
-        "PUERTO BERRIO",
-        "PUERTO TRIUNFO"
+        "Puerto Berrio",
+        "Puerto Triunfo"
       )
     ),
     c("05579", "05591")
@@ -68,7 +68,7 @@ test_that("Divipola department name throws errors", {
 })
 
 test_that("Divipola department name works as expected", {
-  expect_identical(divipola_department_name("05"), "ANTIOQUIA")
+  expect_identical(divipola_department_name("05"), "Antioquia")
 })
 
 test_that("Divipola municipality name throws errors", {
@@ -79,5 +79,5 @@ test_that("Divipola municipality name throws errors", {
 })
 
 test_that("Divipola municipality name works as expected", {
-  expect_identical(divipola_municipality_name("05051"), "ARBOLETES")
+  expect_identical(divipola_municipality_name("05051"), "Arboletes")
 })
