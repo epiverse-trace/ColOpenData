bd57e0781487d638ec8bb3c5dda5a4b26bf5527c
jd-otero <julian-otero@outlook.com>
2024-06-12 12:42:49 -0500

    Change dynamic sized data.frame
    Also, include the initial date for climate data consultation and separate climate assertion function

--- R/download_climate.R
+++ R/download_climate.R
@@ -8,9 +8,9 @@
 #' @param code character with the DIVIPOLA code for the area (2 digits for
 #' departments and 5 digits for municipalities)
 #' @param start_date character with the first date to consult in the format
-#' \code{"YYYY-MM-DD"}
+#' \code{"YYYY-MM-DD"}. (First available date is \code{"1920-01-01"})
 #' @param end_date character with the last date to consult in the format
-#' \code{"YYYY-MM-DD"} (Last available date is \code{"2023-05-31"})
+#' \code{"YYYY-MM-DD"}. (Last available date is \code{"2023-05-31"})
 #' @param tag character containing climate tag to consult
 #'
 #' @examples
@@ -23,6 +23,7 @@
 #' @export
 download_climate <- function(code, start_date, end_date, tag) {
   checkmate::assert_character(code)
+  args <- check_climate_args(start_date, end_date, tag)
 
   if (nchar(code) == 5) {
     # Municipalities have a five digit code
@@ -51,9 +52,9 @@ download_climate <- function(code, start_date, end_date, tag) {
   }
   climate <- download_climate_geom(
     geometry = filtered_area,
-    start_date = start_date,
-    end_date = end_date,
-    tag = tag
+    start_date = args[["start_date"]],
+    end_date = args[["end_date"]],
+    tag = args[["tag"]]
   )
   return(climate)
 }
@@ -68,9 +69,9 @@ download_climate <- function(code, start_date, end_date, tag) {
 #' @param geometry \code{sf} object containing the geometry for a given ROI.
 #' The geometry can be either a \code{POLYGON} or \code{MULTIPOLYGON}
 #' @param start_date character with the first date to consult in the format
-#' \code{"YYYY-MM-DD"}
+#' \code{"YYYY-MM-DD"}. (First available date is \code{"1920-01-01"})
 #' @param end_date character with the last date to consult in the format
-#' \code{"YYYY-MM-DD"} (Last available date is \code{"2023-05-31"})
+#' \code{"YYYY-MM-DD"}. (Last available date is \code{"2023-05-31"})
 #' @param tag character containing climate tag to consult
 #'
 #' @examples
@@ -89,13 +90,14 @@ download_climate <- function(code, start_date, end_date, tag) {
 download_climate_geom <- function(geometry, start_date, end_date,
                                   tag) {
   checkmate::assert_class(geometry, "sf")
+  args <- check_climate_args(start_date, end_date, tag)
 
   stations <- stations_in_roi(geometry)
   climate_geom <- download_climate_stations(
     stations = stations,
-    start_date = start_date,
-    end_date = end_date,
-    tag = tag
+    start_date = args[["start_date"]],
+    end_date = args[["end_date"]],
+    tag = args[["tag"]]
   )
   return(climate_geom)
 }
@@ -111,9 +113,9 @@ download_climate_geom <- function(geometry, start_date, end_date,
 #' \code{data.frame} must be retrieved from the function
 #' \code{stations_in_roi()}
 #' @param start_date character with the first date to consult in the format
-#' \code{"YYYY-MM-DD"}
+#' \code{"YYYY-MM-DD"}. (First available date is \code{"1920-01-01"})
 #' @param end_date character with the last date to consult in the format
-#' \code{"YYYY-MM-DD"} (Last available date is \code{"2023-05-31"})
+#' \code{"YYYY-MM-DD"}. (Last available date is \code{"2023-05-31"})
 #' @param tag character containing climate tag to consult
 #'
 #' @importFrom magrittr %>%
@@ -142,40 +144,11 @@ download_climate_stations <- function(stations, start_date, end_date, tag) {
     c("codigo", "longitud", "latitud"),
     names(stations)
   )
-
-  # Check dates formats and that the end date is greater than the start date
-  checkmate::assert_character(start_date)
-  checkmate::assert_character(end_date)
-  start_date <- as.POSIXct(start_date,
-    format = "%Y-%m-%d",
-    tz = "UTC"
-  )
-  end_date <- as.POSIXct(end_date,
-    format = "%Y-%m-%d",
-    tz = "UTC"
-  )
-  stopifnot(
-    "`end_date` must greater than `start_date`" =
-      start_date <= end_date,
-    "Dates are not in the expected format ('YYYY-MM-DD')" =
-      !anyNA(c(start_date, end_date))
-  )
-
-  # Check that consulted tag exists
-  checkmate::assert_character(tag, len = 1L)
-  tag <- toupper(tag)
-  ideam_tags <- c(
-    "TSSM_CON", "THSM_CON", "TMN_CON", "TMX_CON",
-    "TSTG_CON", "HR_CAL", "HRHG_CON", "TV_CAL",
-    "TPR_CAL", "PTPM_CON", "PTPG_CON", "EVTE_CON",
-    "FA_CON", "NB_CON", "RCAM_CON", "BSHG_CON",
-    "VVAG_CON", "DVAG_CON", "VVMXAG_CON", "DVMXAG_CON"
-  )
-  checkmate::assert_choice(tag, ideam_tags)
+  args <- check_climate_args(start_date, end_date, tag)
 
   path_data <- retrieve_climate_path()
-  path_stations <- paste0(tag, "@", stations$codigo, ".data")
-  climate_data <- data.frame()
+  path_stations <- paste0(args[["tag"]], "@", stations$codigo, ".data")
+  climate_data <- list()
   for (i in seq_along(path_stations)) {
     dataset_path <- file.path(path_data, path_stations[i])
     downloaded_station <- retrieve_climate(dataset_path)
@@ -183,8 +156,8 @@ download_climate_stations <- function(stations, start_date, end_date, tag) {
     if (!rlang::is_empty(downloaded_station)) {
       station_filtered <- downloaded_station %>%
         dplyr::filter(
-          .data$date >= start_date,
-          .data$date <= end_date + 1
+          .data$date >= args[["start_date"]],
+          .data$date <= args[["end_date"]] + 1
         )
       n_obs <- nrow(station_filtered)
       if (n_obs > 0) {
@@ -194,14 +167,15 @@ download_climate_stations <- function(stations, start_date, end_date, tag) {
           latitude = rep(stations$latitud[i], n_obs),
           date = format(station_filtered$date, "%Y-%m-%d"),
           hour = format(station_filtered$date, "%H:%M:%S"),
-          tag = rep(tag, n_obs),
+          tag = rep(args[["tag"]], n_obs),
           value = station_filtered$value,
           stringsAsFactors = FALSE
         )
-        climate_data <- rbind(climate_data, station_obs)
+        climate_data[[length(climate_data) + 1]] <- station_obs
       }
     }
   }
+  climate_data <- dplyr::bind_rows(climate_data)
   # If none of the stations provided data
   if (nrow(climate_data) == 0) {
     stop("There is no available information for the requested dates")
@@ -265,4 +239,67 @@ stations_in_roi <- function(geometry) {
     stop("There are no stations in the specified ROI")
   }
   return(stations_in_roi)
+}
+
+#' Check arguments in climate functions
+#'
+#' @description
+#' Climate functions have three common arguments: \code{start_date},
+#' \code{end_date} and \code{tag}. This function checks that \code{start_date}
+#' and \code{end_date} can be converted to date using the format "YYYY-MM-DD",
+#' that \code{end_date} is greater than \code{start_date}, and that the
+#' \code{tag} requested exists
+#'
+#' @param start_date character with the first date to consult in the format
+#' \code{"YYYY-MM-DD"}. (First available date is \code{"1920-01-01"})
+#' @param end_date character with the last date to consult in the format
+#' \code{"YYYY-MM-DD"}. (Last available date is \code{"2023-05-31"})
+#' @param tag character containing climate tag to consult
+#'
+#' @return list with the arguments in the needed formats. If the input is
+#' invalid an error will be thrown
+#'
+#' @keywords internal
+check_climate_args <- function(start_date, end_date, tag) {
+  # Check dates formats and that the end date is greater than the start date
+  checkmate::assert(
+    checkmate::check_class(start_date, "character"),
+    checkmate::check_class(start_date, c("POSIXct", "POSIXt"))
+  )
+  checkmate::assert(
+    checkmate::check_class(end_date, "character"),
+    checkmate::check_class(end_date, c("POSIXct", "POSIXt"))
+  )
+  start_date <- as.POSIXct(start_date,
+    format = "%Y-%m-%d",
+    tz = "UTC"
+  )
+  end_date <- as.POSIXct(end_date,
+    format = "%Y-%m-%d",
+    tz = "UTC"
+  )
+  stopifnot(
+    "`end_date` must greater than `start_date`" =
+      start_date <= end_date,
+    "Dates are not in the expected format ('YYYY-MM-DD')" =
+      !anyNA(c(start_date, end_date))
+  )
+
+  # Check that consulted tag exists
+  checkmate::assert_character(tag, len = 1L)
+  tag <- toupper(tag)
+  ideam_tags <- c(
+    "TSSM_CON", "THSM_CON", "TMN_CON", "TMX_CON",
+    "TSTG_CON", "HR_CAL", "HRHG_CON", "TV_CAL",
+    "TPR_CAL", "PTPM_CON", "PTPG_CON", "EVTE_CON",
+    "FA_CON", "NB_CON", "RCAM_CON", "BSHG_CON",
+    "VVAG_CON", "DVAG_CON", "VVMXAG_CON", "DVMXAG_CON"
+  )
+  checkmate::assert_choice(tag, ideam_tags)
+  args <- list(
+    start_date = start_date,
+    end_date = end_date,
+    tag = tag
+  )
+  return(args)
 }
