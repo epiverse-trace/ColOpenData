c56a7a9a7bac298b7b1659995744f09a790e7fc7
macataci <mc.tavera@uniandes.edu.co>
2024-10-01 00:19:36 -0500

    updates in disclaimers

--- R/download_demographic.R
+++ R/download_demographic.R
@@ -27,14 +27,15 @@ download_demographic <- function(dataset) {
   message(strwrap(
     prefix = "\n", initial = "",
     c(
-      "ColOpenData provides open data derived from IDEAM and DANE but with
-      modifications for specific functional needs. These changes may alter
-      the structure, format, or content, meaning the data does not reflect the
-      official dataset. The package is developed independently, with no
-      endorsement or involvement from these institutions or any Colombian
-      government body. The authors of ColOpenData are not liable for how users
-      utilize the data, and users are responsible for any outcomes from their
-      use or analysis of the data.",
+      "ColOpenData provides open data derived from The National Administrative
+      Department of Statistics (DANE), and the Institute of Hydrology,
+      Meteorology and Environmental Studies (IDEAM)but with modifications for
+      specific functional needs. These changes may alter the structure, format,
+      or content, meaning the data does not reflect the official dataset. The
+      package is developed independently, with no endorsement or involvement 
+      from these institutions or any Colombian government body. The authors of 
+      ColOpenData are not liable for how users utilize the data, and users are 
+      responsible for any outcomes from their use or analysis of the data.", 
       "Stored by Universidad de Los Andes under the Epiverse TRACE iniative."
     )
   ))
